7e4264854cbf949cc7c88d21be578b903374d226
Evan <128929890+Evaxle@users.noreply.github.com>
2026-09-18 17:14:24 -0400

    Make replay directory setting browser safe

--- scripts/ui/settings/ReplayDir.gd
+++ scripts/ui/settings/ReplayDir.gd
@@ -1,7 +1,13 @@
 extends Button
 
 func _pressed():
+	if OS.has_feature("HTML5"):
+		return
 	OS.shell_open(ProjectSettings.globalize_path(Globals.p("user://replays/")))
 
 func _ready():
-	visible = !OS.has_feature("Android")
+	if OS.has_feature("HTML5"):
+		disabled = true
+		text = "Replays are stored in browser storage"
+	else:
+		visible = not OS.has_feature("Android")
