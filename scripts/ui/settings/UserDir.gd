a735d5763781e3737909897f5fe862a141ed1055
Evan <128929890+Evaxle@users.noreply.github.com>
2026-09-18 17:14:18 -0400

    Make user directory setting browser safe

--- scripts/ui/settings/UserDir.gd
+++ scripts/ui/settings/UserDir.gd
@@ -1,10 +1,15 @@
 extends Button
 
 func _pressed():
-	if !OS.has_feature("Android"):
+	if OS.has_feature("HTML5"):
+		return
+	if not OS.has_feature("Android"):
 		OS.shell_open(ProjectSettings.globalize_path(Globals.p("user://")))
 
 func _ready():
-	if OS.has_feature("Android"):
+	if OS.has_feature("HTML5"):
+		disabled = true
+		text = "User data is stored in browser storage"
+	elif OS.has_feature("Android"):
 		disabled = true
 		text = "User folder location: " + Globals.p("user://")
