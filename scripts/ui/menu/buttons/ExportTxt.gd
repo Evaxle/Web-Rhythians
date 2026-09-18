f9f2f3bf1fa3cb11834a34406a7126b9292fec1f
Evan <128929890+Evaxle@users.noreply.github.com>
2026-09-18 17:15:19 -0400

    Hide native map export in browser client

--- scripts/ui/menu/buttons/ExportTxt.gd
+++ scripts/ui/menu/buttons/ExportTxt.gd
@@ -2,16 +2,21 @@ extends Button
 
 var debounce = false
 
+func _ready():
+	if OS.has_feature("HTML5"):
+		visible = false
+
 func _pressed():
-	if debounce or !Rhythia.selected_song: return
+	if OS.has_feature("HTML5") or debounce or !Rhythia.selected_song:
+		return
 	debounce = true
 	text = "Saving"
 	$SaveFile.set_initial_path("~/Downloads/%s.txt" % Rhythia.selected_song.id)
 	$SaveFile.show()
 	var path = yield($SaveFile,"file_selected")
 	disabled = true
-	Rhythia.selected_song.export_text(path)
+	if str(path) != "":
+		Rhythia.selected_song.export_text(path)
 	text = "Export map data"
 	disabled = false
 	debounce = false
-
