b6f4e76a354c91a1b1711d3c22e655a08c5e6fce
Evan <128929890+Evaxle@users.noreply.github.com>
2026-09-18 17:37:33 -0400

    Expose persistent settings save in browser

--- scripts/ui/settings/SaveSettings.gd
+++ scripts/ui/settings/SaveSettings.gd
@@ -2,10 +2,14 @@ extends Button
 
 func _pressed():
 	Rhythia.save_settings()
+	if OS.has_feature("HTML5"):
+		WebPortal.persist_user_data()
 
 func _exit_tree():
 	if !OS.has_feature("debug"):
 		Rhythia.save_settings()
+		if OS.has_feature("HTML5"):
+			WebPortal.persist_user_data()
 
 func _ready():
-	visible = OS.has_feature("debug")
+	visible = OS.has_feature("debug") or OS.has_feature("HTML5")
