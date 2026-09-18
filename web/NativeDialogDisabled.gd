d404afde1903400ac3ca21e1f9bd7a757f13eb55
Evan <128929890+Evaxle@users.noreply.github.com>
2026-09-17 20:49:23 -0400

    Match native dialog properties in browser stub

--- web/NativeDialogDisabled.gd
+++ web/NativeDialogDisabled.gd
@@ -1,13 +1,21 @@
 extends Node
+
 signal files_selected(paths)
 signal file_selected(path)
 signal dir_selected(path)
+signal folder_selected(path)
 signal canceled
+
+var title = ""
 var dialog_title = ""
 var filters = []
+var initial_path = ""
 var current_dir = ""
 var current_file = ""
+var multiselect = false
+
 func show():
 	emit_signal("canceled")
+
 func popup():
 	emit_signal("canceled")
